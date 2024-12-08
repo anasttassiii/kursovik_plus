82f13cda0894aa842f82e21d9361ca7800e90bf4
Анастасия Метелкина <nm.met@mail.ru>
2024-12-08 21:34:37 +0700

    Добавление файла для отрисовки шрифта

--- kursovik/main.cpp
+++ kursovik/main.cpp
@@ -1,5 +1,14 @@
 #include "Game.h"
+#include <iostream>
+#include "snake.h"
+#include <vector>
+#include <windows.h>
+#include <conio.h>
+#include <sstream> // ƒл€ преобразовани€ чисел в строки
 #include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <ctime>
+
 
 int main() {
     Game game; // создание игры
